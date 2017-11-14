64536495696bf87e5fa7fc1de10c1483230ff27d
ixnas <sjoerd@b00n.org>
2017-11-14 09:57:14 +0100

    Tabs

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,18 +2,19 @@ import React from 'react';
 import '../styles/styles.css';
 
 class Header extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to
+					reload.
+				</p>
+			</div>
+		);
+	}
 }
 
 export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import './styles/styles.css';
 import Header from './components/Header';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<Header/>, document.getElementById('root'));
