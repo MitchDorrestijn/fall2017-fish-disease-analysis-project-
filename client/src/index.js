92b59e053cf272b4a3a3be8b4f6824abce8d813a
ixnas <sjoerd@b00n.org>
2017-11-15 15:25:07 +0100

    Merge conflict gefixt

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.css';
-<<<<<<< HEAD
-=======
 import 'bootstrap/dist/css/bootstrap.css';
->>>>>>> development
 import App from './components/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
