91c270f6e2aeee7ec8e89328845828ea209ec336
ixnas <sjoerd@b00n.org>
2017-11-15 09:39:36 +0100

    Opzetje navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../styles/styles.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import NavigationBar from './navigation/NavigationBar';
 
-class App extends React.Component {
+export default class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
+				<NavigationBar/>
 			</div>
 		);
 	}
 }
 
-export default Header;
