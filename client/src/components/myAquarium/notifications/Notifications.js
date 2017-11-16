e3f3ecafad5e63376686131ba41c190d5da3e15b
ixnas <sjoerd@b00n.org>
2017-11-16 15:54:21 +0100

    Ik claim notifications

--- client/src/components/myAquarium/notifications/Notifications.js
+++ client/src/components/myAquarium/notifications/Notifications.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default class Notifications extends React.Component {
 	render() {
 		return (
-			<h1>Hier komt content voor Notifications</h1>
+			<div className="notifications">
+			</div>
 		);
 	}
 }
