6d3b7565a108c7736eceb72e2792297160f7b839
ixnas <sjoerd@b00n.org>
2017-11-16 12:32:55 +0100

    Arrow functions in plaats van normale functies

--- client/src/components/modal/ModalBase.js
+++ client/src/components/modal/ModalBase.js
@@ -7,7 +7,7 @@ import Register from './Register';
 import ForgotPassword from './ForgotPassword';
 import ForgotPasswordSend from './ForgotPasswordSend';
 
-class ModalBase extends React.Component {
+export default class ModalBase extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -16,26 +16,24 @@ class ModalBase extends React.Component {
 		}
 	}
 	
-	toggleModal(){
+	toggleModal = () => {
 		this.setState({
 			modal: !this.state.modal
 		});
-	}
+	};
 	
-	changeContent(component){
+	changeContent = (component) => {
 		this.setState({
 			modalContent: component
 		});
-	}
+	};
 	
 	render() {
 		const ModalContent = this.state.modalContent;
 		return (
 			<Modal isOpen={this.state.modal} toggle={() => this.toggleModal()}>
-				<ModalContent Login={Login} Register={Register} ForgotPasswordSend={ForgotPasswordSend} ForgotPassword={ForgotPassword} changeContent={this.changeContent.bind(this)} toggleModal={this.toggleModal.bind(this)}/>
+				<ModalContent Login={Login} Register={Register} ForgotPasswordSend={ForgotPasswordSend} ForgotPassword={ForgotPassword} changeContent={this.changeContent} toggleModal={this.toggleModal}/>
 			</Modal>
 		);
 	}
 }
-
-export default ModalBase;
