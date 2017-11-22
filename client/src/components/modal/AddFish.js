69f08f481dcbdc3766939e3b92db9186fc17bfd8
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2017-11-22 08:52:06 +0100

    Added modal select implementation (not done yet, ready for merge).

--- client/src/components/modal/AddFish.js
+++ client/src/components/modal/AddFish.js
@@ -10,9 +10,27 @@ import {
 	Input
 } from 'reactstrap';
 import Error from './Error';
+import fuzzyFilterFactory from 'react-fuzzy-filter';
+
+
 
 class AddFish extends React.Component {
+	constructor(props){
+		super(props);
+		this.InputFilter = fuzzyFilterFactory().InputFilter;
+		this.FilterResults = fuzzyFilterFactory().FilterResults;
+	}
 	render() {
+
+		const items = [
+      { name: 'first', meta: 'first|123', tag: 'a' },
+      { name: 'second', meta: 'second|443', tag: 'b' },
+      { name: 'third', meta: 'third|623', tag: 'a' },
+    ];
+    const fuseConfig = {
+      keys: ['meta', 'tag']
+    };
+
 		return (
 			<div>
 				<ModalHeader toggle={() => this.props.toggleModal()}>Add fish</ModalHeader>
@@ -24,8 +42,22 @@ class AddFish extends React.Component {
 					<FormGroup>
 						<Label for="addfish">Name of fish:</Label>
 						<InputGroup>
-							<InputGroupAddon><i className="fa fa-fish"/></InputGroupAddon>
-							<Input type="text" id="addFish" placeholder="Type the fish name here"/>
+
+
+							<this.InputFilter debounceTime={200} />
+			 				<div>Any amount of content between</div>
+			 				<this.FilterResults items={items} fuseConfig={fuseConfig}>
+								{filteredItems => {
+					 				return(
+						 				<div>
+							 				{filteredItems.forEach(item => <div>{item.name}</div>)}
+						 				</div>
+					 				)
+				 				}}
+			 				</this.FilterResults>
+
+
+
 						</InputGroup>
 					</FormGroup>
 					<hr/>
