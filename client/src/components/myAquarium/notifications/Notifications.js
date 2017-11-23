94348dbae1d7ba960ee8c0410a000788e6ef6f8c
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2017-11-23 15:19:54 +0100

    Notifications are now fetched from the database.

--- client/src/components/myAquarium/notifications/Notifications.js
+++ client/src/components/myAquarium/notifications/Notifications.js
@@ -3,73 +3,61 @@ import Notification from './Notification';
 import NotificationInfo from './NotificationInfo';
 import NotificationData from './NotificationData';
 import {Col} from 'reactstrap';
+import DataAccess from '../../../scripts/DataAccess';
 
 export default class Notifications extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			notifications: []
+			notifications: [],
+			notificationsFromDB: []
 		};
 	}
-
 	componentWillMount() {
-		this.getNotifications();
+		this.loadNotifications();
 	}
-
-	getNotifications = () => {
-		// Deze notificaties zouden uit de database moeten komen en vervolgens hier geparsed moeten worden.
-		// Er wordt verwacht dat er een for loop gebruikt wordt, hierbij moeten "key" en "id" allebei de iterator van de loop zijn
-		let notifications = [
-			<Notification icon="danger-icon" date="20-11-2017" key={0} id={0} deleteNotification={this.deleteNotification}>
-				<NotificationInfo title="The fish you added is not welcome in your aquarium.">
-					Angels and bettas are both notorious fin nippers,
-					and if you end up with angel fish that decide to
-					pair up, it will probably end up pretty ugly for the
-					betta.
-				</NotificationInfo>
-				<NotificationData>
-					Conflicting fish<br/>
-					- Angel Fish<br/>
-					- Betta
-				</NotificationData>
-			</Notification>,
-			<Notification icon="warning-icon" date="19-11-2017" key={1} id={1} deleteNotification={this.deleteNotification}>
-				<NotificationInfo title="Your water 6.4 pH is not optimal.">
-					Please check your aquarium water to make sure your fish has the optimal living environtment.
-					Adding baking soda will raise the pH, but remember that this will need to be constantly added.
-				</NotificationInfo>
-				<NotificationData>
-					Water pH<br/>
-					- Currently 6.4 pH<br/>
-					- Optimal 8.5 pH
-				</NotificationData>
-			</Notification>,
-			<Notification icon="fish-icon-blue" date="18-11-2017" key={2} id={2} deleteNotification={this.deleteNotification}>
-				<NotificationInfo title="New fish added to aquarium">
-					You have recently added a bluespotted corydoras in aquarium #1, you now have three bluespotted corydoras in total.
-				</NotificationInfo>
-				<NotificationData>
-					New fish<br/>
-					- Bluespotted corydoras<br/>
-					- Three bluespotted corydoras in total
-				</NotificationData>
-			</Notification>,
-			<Notification icon="fish-icon-removedfish" date="17-11-2017" key={3} id={3} deleteNotification={this.deleteNotification}>
-				<NotificationInfo title="Fish removed from aquarium #2 ">
-					You have recently removed one bluespotted corydoras in aquarium #2.
-				</NotificationInfo>
-				<NotificationData>
-					Removed fish<br/>
-					- Bluespotted corydoras<br/>
-					- Two bluespotted corydoras left.
-				</NotificationData>
-			</Notification>,
-		];
-		this.setState ({
-			notifications: notifications
+	loadNotifications = () => {
+		let da = new DataAccess();
+		da.getData ('/notifications', (err, res) => {
+			if (!err) {
+				console.log (res.message);
+				this.setState({notificationsFromDB: res.message});
+				this.getNotifications();
+			} else {
+				console.log("De error is: " + err.message);
+			}
 		});
+	}
+	timeToString = (input) => {
+		let date = new Date (input);
+		let day = date.getDate ();
+		let month = (date.getMonth () + 1).toString (10);
+		let year = date.getFullYear ().toString (10);
+		return `${day}-${month}-${year}`;
 	};
-
+	getNotifications = () => {
+		let notifications = [];
+		for(var key in this.state.notificationsFromDB) {
+    	if(this.state.notificationsFromDB.hasOwnProperty(key)) {
+				console.log(key);
+				notifications.push(
+					<Notification icon="danger-icon" date={this.timeToString(this.state.notificationsFromDB[key].date)} key={parseInt(key,10)} id={parseInt(key,10)} deleteNotification={this.deleteNotification}>
+						<NotificationInfo title="The fish you added is not welcome in your aquarium.">
+							{this.state.notificationsFromDB[key].message}
+						</NotificationInfo>
+						<NotificationData>
+							Conflicting fish<br/>
+							- Angel Fish<br/>
+							- Betta
+						</NotificationData>
+					</Notification>
+				);
+    	}
+		}
+		this.setState({
+			notifications: notifications
+		})
+	}
 	deleteNotification = (key) => {
 		let notifications = this.state.notifications;
 		for (let i = 0; i < notifications.length; i++) {
@@ -81,7 +69,6 @@ export default class Notifications extends React.Component {
 			notifications: notifications
 		});
 	};
-
 	render() {
 		return (
 			<div className="notifications">
