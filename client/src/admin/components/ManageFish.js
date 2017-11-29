0c6b25db0f49201273f5a07973a08c11a2abcb09
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2017-11-29 15:10:04 +0100

    Added search functionallity on the front-end.

--- client/src/admin/components/ManageFish.js
+++ client/src/admin/components/ManageFish.js
@@ -2,19 +2,38 @@ import React from 'react';
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
 import InlineEditable from "react-inline-editable-field";
-
+import { Button, Form, FormGroup, Input } from 'reactstrap';
 
 export default class ManageFish extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			searchTerm: ""
+		}
+	}
 	updateRecord = (recordToUpdate) => {
   	console.log(recordToUpdate);
+	}
+	getSearchTerm = (e) => {
+		e.preventDefault();
+		console.log("SerchTerm: " + this.state.searchTerm);
+	}
+	handleSearchChange = (e) => {
+		this.setState({searchTerm: e.target.value});
 	}
 	render(){
   	return (
 			<div>
-				<Table>
+				<h2>Vissen toevoegen / bewerken / verwijderen</h2>
+				<Form inline className="searchForm" onSubmit={this.getSearchTerm}>
+        	<FormGroup>
+          	<Input type="text" name="searchTerm" placeholder="Wat wil je zoeken?" onChange={this.handleSearchChange} />
+        	</FormGroup>
+					<Button>Zoek nu</Button>
+				</Form>
+				<Table className="table">
 				  <Thead>
 				  	<Tr>
-	            <Th>ID</Th>
 	            <Th>Vis naam</Th>
 	            <Th>Omschrijving</Th>
 	            <Th>Afbeelding</Th>
@@ -22,34 +41,29 @@ export default class ManageFish extends React.Component {
 			    </Thead>
 				    <Tbody>
 				        <Tr>
-				            <Td>1</Td>
-				            <Td><InlineEditable content="uit database" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
-				            <Td>Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large.</Td>
-				            <Td>img/afbeelding.png</Td>
+				            <Td><InlineEditable className="editFocus" content="Potvis" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable className="editFocus" content="Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large." inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable className="editFocus" content="img/afbeelding.png" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
 				        </Tr>
 								<Tr>
-				            <Td>1</Td>
-				            <Td>Potvis</Td>
-				            <Td>Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large.</Td>
-				            <Td>img/afbeelding.png</Td>
+										<Td><InlineEditable content="Potvis" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable content="Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large." inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable content="img/afbeelding.png" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
 				        </Tr>
 								<Tr>
-				            <Td>1</Td>
-				            <Td>Potvis</Td>
-				            <Td>Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large.</Td>
-				            <Td>img/afbeelding.png</Td>
+										<Td><InlineEditable content="Potvis" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable content="Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large." inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable content="img/afbeelding.png" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
 				        </Tr>
 								<Tr>
-				            <Td>1</Td>
-				            <Td>Potvis</Td>
-				            <Td>Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large.</Td>
-				            <Td>img/afbeelding.png</Td>
+										<Td><InlineEditable content="Potvis" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable content="Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large." inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable content="img/afbeelding.png" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
 				        </Tr>
 								<Tr>
-				            <Td>1</Td>
-				            <Td>Potvis</Td>
-				            <Td>Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large.</Td>
-				            <Td>img/afbeelding.png</Td>
+										<Td><InlineEditable content="Potvis" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable content="Amphiliids are generally small catfish with tapering, elongated bodies. The pectoral and ventral fins are large." inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
+				            <Td><InlineEditable content="img/afbeelding.png" inputType="text" onBlur={(recordToUpdate) => {this.updateRecord(recordToUpdate)}}/></Td>
 				        </Tr>
 				    </Tbody>
 				</Table>
