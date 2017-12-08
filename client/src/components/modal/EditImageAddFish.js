f0a8a0d662c825dd146c3c2e9668b9722118774f
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2017-12-08 08:52:25 +0100

    Added setup for file-upload.

--- client/src/components/modal/EditImageAddFish.js
+++ client/src/components/modal/EditImageAddFish.js
@@ -9,11 +9,19 @@ import {
 	Form,
 	FormText
 } from 'reactstrap';
+import ReactFileReader from 'react-file-reader';
 
 
 export default class EditImageAddFish extends React.Component {
-	addFishImage = () => {
+	addFishImage = (e) => {
+		e.preventDefault();
+		 // let formData = new FormData();
+		 // formData.append('fishImage', document.getElementById("fishImage").files[0], 'naamVanGeuploadeAfbeelding.jpg');
+     //
+     //
+		 // formData.append('fishImage', document.getElementById("fishImage").prop('files')[0]);
 
+		 //console.log(formData);
 		// $('#uploadBtn').click((e) => {
     //                let formData = new FormData();
     //                formData.append('fishImage', $('#fishImage').prop('files')[0]);
@@ -31,17 +39,29 @@ export default class EditImageAddFish extends React.Component {
     //            })
 
 	}
+
+	handleFiles = (files) => {
+  console.log(files)
+}
+
+
 	render() {
 		return (
 			<div>
 				<ModalHeader toggle={() => this.props.toggleModal()}>Change/add fish image</ModalHeader>
 				<ModalBody>
 					<Form onSubmit={this.addFishImage}>
-						<FormGroup>
+						{/* <FormGroup>
           		<Label for="fishImage">Image</Label>
           		<Input id="fishImage" type="file" name="fishImage" />
           		<FormText color="muted">Images can be uploaded in .jpg and .png.</FormText>
-        		</FormGroup>
+        		</FormGroup> */}
+
+						<ReactFileReader handleFiles={this.handleFiles}>
+							<button className='btn'>Upload</button>
+						</ReactFileReader>
+
+
 		        <Button>Submit</Button>
 		      </Form>
 				</ModalBody>
