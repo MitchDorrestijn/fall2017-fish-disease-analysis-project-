0183d7ae93c4ba7a5a593fc18a0934914d394937
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2017-12-08 14:12:46 +0100

    Added image validation.

--- client/src/components/modal/EditImageAddFish.js
+++ client/src/components/modal/EditImageAddFish.js
@@ -14,34 +14,46 @@ import {
 import ReactFileReader from 'react-file-reader';
 
 export default class EditImageAddFish extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			error: ""
+		}
+	}
 	addFishImage = (e) => {
 		e.preventDefault();
 		const inputElement = document.getElementById('fishImage');
-		const file = inputElement.files[0];
-		let formData = new FormData();
-		formData.append('image', file);
-		firebase.auth ().currentUser.getIdToken ().then ((result) => {
-			const token = result;
-			fetch(`http://localhost:5000/api/species/${this.props.customProps.entry.id}/upload`, {
-	   			method: 'POST',
-	   			headers: {
-			 			'Authorization': 'Token ' + token
-	   			},
-		 			mode: 'cors',
-	   			body: formData
-			}).then(() => {
-				this.props.customProps.refreshPage();
-				this.props.toggleModal();
-			}).catch ((error) => {
-					console.log(error);
+
+		if((/\.(gif|jpg|jpeg|tiff|png)$/i).test(inputElement.value)){
+			const file = inputElement.files[0];
+			let formData = new FormData();
+			formData.append('image', file);
+			firebase.auth ().currentUser.getIdToken ().then ((result) => {
+				const token = result;
+				fetch(`http://localhost:5000/api/species/${this.props.customProps.entry.id}/upload`, {
+		   			method: 'POST',
+		   			headers: {
+				 			'Authorization': 'Token ' + token
+		   			},
+			 			mode: 'cors',
+		   			body: formData
+				}).then(() => {
+					this.props.customProps.refreshPage();
+					this.props.toggleModal();
+				}).catch ((error) => {
+						this.setState({error: "An error occurred"});
+				});
 			});
-		});
+		} else {
+			this.setState({error: "Please upload a .GIF, .JPG, .JPEG, .TIFF or .PNG image."})
+		}
 	}
 	render() {
 		return (
 			<div>
 				<ModalHeader toggle={() => this.props.toggleModal()}>Change/add fish image</ModalHeader>
 				<ModalBody>
+					<p className="error">{this.state.error}</p>
 					<Form onSubmit={this.addFishImage}>
 						<FormGroup>
           		<Label for="fishImage">Image</Label>
