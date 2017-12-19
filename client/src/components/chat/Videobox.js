0e0957f4b688edec90591ff5d59c7ae8240ba1b3
Unknown <coen_severein@hotmail.com>
2017-12-19 11:02:12 +0100

    Mirrored webcam, added text/video support, added progressbar

--- client/src/components/chat/Videobox.js
+++ client/src/components/chat/Videobox.js
@@ -33,8 +33,18 @@ export default class Videobox extends React.Component {
 							<video className="myCam" id="myCam" autoPlay muted></video>
 							<div className="videoOverlay">
 								<div className="videoOverlayText">
-									<Button id="toggleAudio" onClick={() => this.toggleAudio()} className="btn-circle" color="primary"><i className="fa fa-microphone-slash"/></Button>
-									<Button id="toggleVideo" onClick={() => this.toggleVideo()} className="btn-circle" color="primary"><i className="fa fa-video-camera"/></Button>
+									{
+										(this.props.stream !== null) ?
+										([
+											<Button key={0} id="toggleAudio" onClick={() => this.toggleAudio()} className="btn-circle" color="primary"><i className="fa fa-microphone-slash"/></Button>,
+											<Button key={1} id="toggleVideo" onClick={() => this.toggleVideo()} className="btn-circle" color="primary"><i className="fa fa-video-camera"/></Button>,
+											<Button key={2} id="stopWebcam" onClick={() => this.props.stopWebcam()} className="btn-circle" color="danger"><i className="fa fa-phone fa-rotate-90"/></Button>
+										])
+										:
+										(
+											<Button id="startWebcam" onClick={() => this.props.startWebcam()} className="btn-circle" color="success"><i className="fa fa-phone"/></Button>
+										)
+									}
 								</div>
 							</div>
 						</Row>
