53df252a98c265f26e18a0d39895e88a0d30799b
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2018-01-08 10:10:11 +0100

    Current progress.

--- client/src/components/myAquarium/myFish/MyFish.js
+++ client/src/components/myAquarium/myFish/MyFish.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from '../../base/Card';
-import {CardText, CardTitle, CardSubtitle} from 'reactstrap';
+import {CardText, CardTitle, CardSubtitle, Button} from 'reactstrap';
 import ActionButton from '../../base/ActionButton';
 import {Link} from 'react-router-dom';
 import AddFish from '../../modal/AddFish';
@@ -15,8 +15,10 @@ export default class MyFish extends React.Component {
 			fishData: [],
 			createdAquariums: "",
 			currentAquarium: "",
-			currentAquariumId: ""
+			currentAquariumId: "",
+			showCRUDbtns: false
 		}
+		this.baseState = this.state;
 	}
 	componentWillMount(){
 		this.renderAquariums();
@@ -42,45 +44,70 @@ export default class MyFish extends React.Component {
 						color="primary btn-transparent"
 						buttonText={name}
 						onClickAction={() => this.showSelectedCategory(id)}
+						id={id}
 					/>
 				);
 			}
 		}
 		return (
-			<div>
+			<div className="btn-wrapper">
 				{buttons}
 			</div>
 		)
 	}
+
 	showSelectedCategory = (aquariumName) => {
+		for(let i=0; i<this.state.createdAquariums.length; i++){
+			document.getElementById(this.state.createdAquariums[i].id).classList.remove("focus");
+		}
+		document.getElementById(aquariumName).className += " focus";
+
 		let da = new DataAccess();
 		da.getData(`/aquaria/${aquariumName}/fish`,  (err, res) => {
 			if (!err) {
-				this.setState({ currentAquarium: res.message, currentAquariumId: aquariumName });
+				this.setState({ currentAquarium: res.message, currentAquariumId: aquariumName, showCRUDbtns: true });
 			}
 		});
   }
+	objectLength = (obj) => {
+	  let result = 0;
+	  for(let prop in obj) {
+	    if (obj.hasOwnProperty(prop)) {
+	      result++;
+	    }
+	  }
+	  return result;
+	}
 	filterFish = () => {
-		if (this.state.currentAquarium) {
-			const {fish} = this.state.currentAquarium;
-			const result = fish.map ((elem) => {
-				return (
-					<Card key={elem.id} image={elem.species.imageUrl}>
-						<CardTitle>{elem.species.name}</CardTitle>
-						<CardSubtitle>{elem.species.additional}</CardSubtitle>
-						<CardText>{elem.species.info}</CardText>
-					</Card>
-				);
-			});
-			return result;
+		let amountOfFishInAqauria = this.objectLength(this.state.currentAquarium.fish);
+		if(amountOfFishInAqauria > 0){
+			if (this.state.currentAquarium) {
+				const {fish} = this.state.currentAquarium;
+				const result = fish.map ((elem) => {
+					return (
+						<Card key={elem.id} image={elem.species.imageUrl}>
+							<CardTitle>{elem.species.name}</CardTitle>
+							<CardSubtitle>{elem.species.additional}</CardSubtitle>
+							<CardText>{elem.species.info}</CardText>
+						</Card>
+					);
+				});
+				return result;
+			} else {
+				return null;
+			}
 		} else {
-			return null;
+			if(this.state.currentAquarium !== ""){
+				return <p><span className="inline-text">You did not add any fish here, why don't you</span><a className="inline-btn" onClick={this.showAddFishModel} color="link">add one</a></p>
+			} else {
+				return <p>Please select an aquarium.</p>;
+			}
 		}
 	}
 	showAddFishModel = (e) => {
 		e.preventDefault ();
 		console.log(this.state.currentAquariumId);
-		this.props.openModal(AddFish, {refreshPage: () => this.showSelectedCategory (this.state.currentAquariumId), currentAquarium: this.state.currentAquariumId});
+		this.props.openModal(AddFish, {refreshPage: () => this.showSelectedCategory (this.state.currentAquariumId), currentAquarium: this.state.currentAquariumId, fishInAquaria: this.state.currentAquarium});
 	};
 	showAddAquariumModel = (e) => {
 		e.preventDefault ();
@@ -88,18 +115,28 @@ export default class MyFish extends React.Component {
 	}
 	showRemoveAquariumModel = (e) => {
 		e.preventDefault ();
-		this.props.openModal(RemoveAquarium, {refreshPage: this.renderAquariums});
+		this.props.openModal(RemoveAquarium, {refreshPage: this.renderAquariums, currentAquarium: this.state.currentAquariumId, reset: this.resetOnRemove});
+	}
+	resetOnRemove = () => {
+		this.setState(this.baseState);
 	}
 	render() {
 		return (
 			<div className="container">
+				{console.log(this.state.currentAquariumId)}
 				<div className="row inner-content">
 					<h2>My fish</h2>
 					<div className="addBtns">
 						{this.createFilterButtons()}
-						<Link to="" onClick={this.showAddAquariumModel}>+ Add aquarium</Link>
-						<Link to="" onClick={this.showAddFishModel}>+ Add fish</Link>
-						<Link to="" onClick={this.showRemoveAquariumModel}>- Remove aquarium</Link>
+						<div className="action-btn-wrapper">
+							{this.state.showCRUDbtns && (
+								<div className="fishCRUDbtns">
+									<Link to="" onClick={this.showAddFishModel}>+ Add fish to this aquarium</Link>
+									<Link to="" onClick={this.showRemoveAquariumModel}>- Remove this aquarium</Link>
+								</div>
+							)}
+							<Link to="" onClick={this.showAddAquariumModel}>+ Add new aquarium</Link>
+						</div>
 					</div>
 					<div className="card-columns">
 						{this.filterFish()}
