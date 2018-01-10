5f875e1c6e9c1f04dfb5f19351251664542134d0
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2018-01-10 15:35:45 +0100

    Navbar renders the analyse component.

---
+++ client/src/components/analyse/Analyse.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Analyse extends React.Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(
+			<div>
+				<p>aaaa</p>
+			</div>
+		);
+	}
+}
