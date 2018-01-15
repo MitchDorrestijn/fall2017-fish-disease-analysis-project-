8ef36f6f1b0c4a40f45b4a3f895e5ef0a524b033
Weijland <jaapweijland@hotmail.com>
2018-01-15 15:33:50 +0100

    FIX 0 BYTES ERROR

--- firebase/functions/routes/question.js
+++ firebase/functions/routes/question.js
@@ -18,17 +18,6 @@ let symptomenEnZiektesPath = path.join(os.tmpdir(), 'symptomenEnZiektes.xlsx');
 let kruisjes_path;
 let analysis;
 
-const voorbeeldAntwoord = [
-	{
-		question: "Select pictures that matches the symptoms on your fish (multiple possible)",
-		answers: ["White grub disease (encapsulated worm larvae, NO ICH)"]
-	},
-	{
-		question: 1,
-		answers: ["Cotton growth"]
-	}
-];
-
 const getExcelFileLocalPath = (fileName) => {
     return path.join(os.tmpdir(), fileName + '.xlsx');
 }
@@ -37,19 +26,43 @@ const getExcelFileRemotePath = (fileName) => {
     return '/excel/' + fileName + '.xlsx';
 }
 
-const getExcelFile = (fileName, force) => {
-    const localPath = getExcelFileLocalPath(fileName);
-    if (fs.existsSync(localPath) && !force) {
-        console.log('Getting file ' + fileName + ' from local file system.');
-        return Promise.resolve(fs.readFileSync(localPath));
-    }
+const fileExists = (fileName) => {
+    return new Promise ((resolve, reject) => {
+        fs.access (getExcelFileLocalPath(fileName), fs.constants.R_OK, (err) => {
+            if (err) {
+                resolve(false);
+            } else {
+                const stats = fs.statSync(getExcelFileLocalPath(fileName));
+                resolve(!stats.size == 0);
+            }
+        });
+    });
+}
 
-    console.log('File ' + fileName + ' not in memory, downloading it.');
-    return bucket.file(getExcelFileRemotePath(fileName)).download({
-        destination: getExcelFileLocalPath(fileName)
-    }).then(() => {
-        return Promise.resolve(fs.readFileSync(getExcelFileLocalPath(fileName)));
-    })
+const getExcelFile = (fileName, force) => {
+    console.log ("SJOERD!!@#@#$@#$@");
+    const localPath = getExcelFileLocalPath(fileName);
+
+    return fileExists(fileName).then((exists) => {
+        if (!force && exists) {
+            console.log('Getting file ' + fileName + ' from local file system.');
+
+            return Promise.resolve(fs.readFileSync(localPath));
+        } else {
+            console.log('File ' + fileName + ' not in memory, downloading it.');
+            
+            return bucket.file(getExcelFileRemotePath(fileName)).download({
+                destination: localPath
+            }).then(() => {
+                return Promise.resolve(fs.readFileSync(localPath));
+            }).catch((err) => {
+               return Promise.reject(err);
+               console.log(err);
+            });
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
 }
 
 const p1 = getExcelFile('symptomenEnZiektes');
@@ -57,20 +70,29 @@ const p2 = getExcelFile('Vraagziektekruisjes');
 
 // First download files, then call the shit.
 Promise.all([p1, p2]).then(() => {
+    console.log('promises resolved');
     let config = AnalysisFactory._parseConfig('config.json');
     config.diseasesAndSymptoms.file = getExcelFileLocalPath('symptomenEnZiektes');
     config.questions.questionsAndFollowUpQuestions.file = getExcelFileLocalPath('Vraagziektekruisjes');
     config.questions.questionAnswersAndSymptoms.file = getExcelFileLocalPath('Vraagziektekruisjes');
     analysis = AnalysisFactory.getAnalysisWithCustomConfig(config);
-
-    //console.log(analysis.getResults(voorbeeldAntwoord));
-    //console.log(analysis.getNextQuestions(voorbeeldAntwoord)[0]);
 }).catch((err) => {
     console.log(err);
 })
 
 router.get('/questions/shallow', (req, res) => {
     res.send(analysis.getFirstQuestions());
+});
+
+router.get('/questions/refresh', (req, res) => {
+    const p1 = getExcelFile('symptomenEnZiektes', true);
+    const p2 = getExcelFile('Vraagziektekruisjes', true);
+
+    Promise.all([p1, p2]).then(() => {
+        res.send('Cache cleared');
+    }).catch((err) => {
+        res.status(400).send(err.message);
+    });
 });
 
 router.post('/questions/deep', (req, res) => {
