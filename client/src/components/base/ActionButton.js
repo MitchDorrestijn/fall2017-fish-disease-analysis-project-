f83d4fe1ceef2ad0138e726f1ac0207ec66b762f
Roy Meijer <R.Meijer2@student.han.nl>
2018-01-19 10:18:39 +0100

    Button 1 second disabled

--- client/src/components/base/ActionButton.js
+++ client/src/components/base/ActionButton.js
@@ -6,11 +6,18 @@ export default class ActionButton extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+		  	disabled: false,
 			redirect: null
 		}
 	}
 
 	onClickAction = () => {
+	  	// Setting the button on disabled for a second, so client side won't send to many ajax requests. Not a very elegant solution
+	  	this.setState({disabled: true});
+	  	setTimeout(() => {
+		  this.setState({disabled: false});
+		},1000);
+
 		if (this.props.linkTo){
 	        this.setState ({redirect: <Redirect to={this.props.linkTo} />});
 		} else if (this.props.onClickAction) {
@@ -21,7 +28,7 @@ export default class ActionButton extends React.Component {
 	render(){
 		return(
 			<div>
-				<Button id={this.props.id} outline color={this.props.color} onClick={this.onClickAction}>{this.props.buttonText}</Button>
+				<Button disabled={this.state.disabled} id={this.props.id} outline color={this.props.color} onClick={this.onClickAction}>{this.props.buttonText}</Button>
 				{this.state.redirect}
 			</div>
 		);
