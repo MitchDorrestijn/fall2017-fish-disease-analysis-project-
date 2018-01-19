25d0996eb0311fdeaec572d351223530cf18aa9b
Roy Meijer <R.Meijer2@student.han.nl>
2018-01-19 14:08:46 +0100

    Reverted timeslot time, added screenshots 2 software guidebook

--- client/src/admin/components/ManageAgenda/ManageAgenda.js
+++ client/src/admin/components/ManageAgenda/ManageAgenda.js
@@ -74,6 +74,8 @@ export default class ManageAgenda extends Component {
 		let da = new DataAccess();
 
 		da.getData('/admin/appointments', (err, res) => {
+		  console.log(res);
+		  console.log(err);
 			if (!err) {
 				let resultsFromDB = res.message;
 				resultsFromDB.sort (this.sortAppointmentsByDate);
